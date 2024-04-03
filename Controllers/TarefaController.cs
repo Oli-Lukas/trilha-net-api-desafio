d39b1ed01ccb6de711965dedb3f3fe21d428929d
Lucas Oliveira <lucas.g.oliveira1998@outlook.com>
2024-04-03 00:23:58 -0300

    Implementando rota HttpGet '/Tarefa/{id}'

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -18,10 +18,14 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("{id}")]
         public IActionResult ObterPorId(int id)
         {
-            // TODO: Buscar o Id no banco utilizando o EF
-            // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
-            // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+            // [X] TODO: Buscar o Id no banco utilizando o EF
+            var tarefa = _context.Tarefas.Find(id);
+
+            // [X] TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
+            if (tarefa == null) return NotFound();
+
+            // [X] caso contrário retornar OK com a tarefa encontrada
+            return Ok(tarefa);
         }
 
         [HttpGet("ObterTodos")]
